79859dc64e16328f9ea196a666325e99b88f259b
Diya Shrestha <diyashrestha2000@gmail.com>
2024-01-04 04:33:32 +0545

    added xs for responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,17 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    screens: {
+      xs: "150px",
+      sm: "576px",
+      // => @media (min-width: 576px) { ... }
+
+      md: "960px",
+      // => @media (min-width: 960px) { ... }
+
+      lg: "1200px",
+      // => @media (min-width: 1440px) { ... }
+    },
   },
   plugins: [],
 };
